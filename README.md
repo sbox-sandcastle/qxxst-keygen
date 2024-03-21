0224648de5b17b86deb4014e76c1da2a8fbcfe33
qxxst <qxxst@proton.me>
2024-03-21 15:54:02 -0500

    Removed Herobrine

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Sandbox Key Generator
 The #1 tool for generating real sandbox keys.
 
-## Credits
-<a href="https://github.com/sbox-sandcastle/qxxst-keygen/graphs/contributors"><img src="https://contrib.rocks/image?repo=sbox-sandcastle/qxxst-keygen"></a>
-- Poppins by Google Fonts.
-- Key icon by favicon.io.
+This software is copyrighted. It is illegal to redistribute on any website, including and especially discord, without our permission. For more information please see the [license](https://github.com/sbox-sandcastle/qxxst-keygen/blob/main/LICENSE).
+
+We like having this tool be open source because it allows people to learn from it, but small children who don't understand copyright law require a proprietary license to be necessary.
--- docs/key.js
+++ docs/key.js
@@ -24,74 +24,29 @@ function checkLength(key) {
     const expectedLength = sampleKey.length;
     return key.length == expectedLength;
 }
-function realKey() {
-    const sampleKeys = [
-        "C9C97725-82EE-94F6-6F1D-CF72BBDA21E4",
-        "520DE514-ED2E-1BBD-08B1-F459BA3B1E3E",
-        "3DBE5467-E9CB-CC9F-1A46-2FC65BC5C50B",
-        "7EFA664D-F23A-AF70-ACEE-00E314055398",
-        "79A75C3B-645E-ADD0-0C3C-6C9257C48570",
-        "617C8B5A-8BB9-0AEF-4C46-87D3544B1111",
-        "52D1A71B-48AC-4F88-1E7C-5C7E28181834",
-        "2FF4AFDE-0E5C-B7DE-BDA9-841C76BDEB2F",
-        "354FFAE7-18C3-07D6-D987-0E61567FDA02",
-        "23769D96-358D-291A-D579-01D32186984C",
-        "6B9B706C-1422-4818-94E6-C36B8E64A539",
-        "C06FDC5C-A5EB-4FA4-88D0-1D82BC09D65E",
-        "480EB61B-CEFB-49F6-BFC5-68079929233A",
-        "A5EC2713-F5B7-43FA-B8CC-41D321381D53",
-        "DC05F1F6-8568-45EF-A094-00F7E614B668",
-        "877B7F40-9182-4EF9-B0B7-04EF609B4479",
-        "1482CF5B-758A-4BA9-9814-5E07588CF686",
-        "3DC554BA-48EC-4399-8FA0-FB88EBC56D37",
-        "37B20145-947E-42EA-8541-5475D9DCD1C7",
-        "26D3A7DA-67B6-4EF2-99D5-00CB8EBF6D5E",
-        "9EABDDBE-994B-4BD2-A92A-389BC00437DA",
-        "D8906A38-A156-47AB-9EDB-CF56DFD4C6C4",
-        "9295AD6D-3835-48F2-BC5A-90A4B2935283",
-        "5C577A2B-4795-4E22-87E3-E30BB7F92F93",
-        "6D03515F-23C3-4B2B-9EFB-D9D9C1648BA7",
-        "9746D022-6A2F-48C9-BB39-DB92768627CC",
-        "505CB7FC-9589-4E8D-9140-31B5B76394E9",
-        "C03AEBA7-C24C-4CE6-8FB0-FF6102B15D67",
-        "09D8C2A2-DF18-4B56-8C1C-DFC8E0277ED7",
-        "83D7C56F-550C-44B4-A454-C6EB0D41DB74",
-        "BD7F4E75-5FCC-4F8E-8624-D64AF9BBC18C",
-        "BF565CEE-66BA-4710-A9DB-EA828ED9D312",
-        "BCF70287-E27D-4709-89EA-7EEAD0C533B3",
-        "1BF8212F-E3DC-460C-A29A-53CE1EA7F88A",
-        "9737A60D-986B-4009-B759-D97743072D7D"
-    ];
-    return sampleKeys[Math.floor(Math.random() * sampleKeys.length)];
-}
-function key(method) {
+function key() {
     let key = "";
-    if (method == 1) {
-        const dash = "-";
-        for (let i = 0; i < 8; i++) {
-            key += getRandomCharacter();
-        }
-        key += dash;
-        for (let i = 0; i < 4; i++) {
-            key += getRandomCharacter();
-        }
-        key += dash;
-        key += "4"; // Character 13 is always 4.
-        for (let i = 0; i < 3; i++) {
-            key += getRandomCharacter();
-        }
-        key += dash;
-        key += getUniqueCharacter();
-        for (let i = 0; i < 3; i++) {
-            key += getRandomCharacter();
-        }
-        key += dash;
-        for (let i = 0; i < 12; i++) {
-            key += getRandomCharacter();
-        }
+    const dash = "-";
+    for (let i = 0; i < 8; i++) {
+        key += getRandomCharacter();
     }
-    if (method == 2) {
-        key = realKey();
+    key += dash;
+    for (let i = 0; i < 4; i++) {
+        key += getRandomCharacter();
+    }
+    key += dash;
+    key += "4"; // Character 13 is always 4.
+    for (let i = 0; i < 3; i++) {
+        key += getRandomCharacter();
+    }
+    key += dash;
+    key += getUniqueCharacter();
+    for (let i = 0; i < 3; i++) {
+        key += getRandomCharacter();
+    }
+    key += dash;
+    for (let i = 0; i < 12; i++) {
+        key += getRandomCharacter();
     }
     if (checkLength(key)) {
         return key;
@@ -144,7 +99,7 @@ function applyRateLimit() {
     outputField.innerHTML = rateLimitText;
     generateButton.disabled = true;
 }
-function generateKey(method) {
+function generateKey() {
     const outputField = document.getElementById("output");
     const generateButton = document.getElementById("generateButton");
     if (!rateLimited) {
@@ -156,7 +111,7 @@ function generateKey(method) {
             document.getElementsByTagName("button")[0].classList.add("qm-fade-in-out");
             let returnText = "";
             if (weFeelLikeIt) {
-                returnText = key(method);
+                returnText = key();
             }
             else {
                 returnText = errorText;
